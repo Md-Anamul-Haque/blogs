6c7990d40313100adde2656a7ea118c5bbcb1af4
md-anamul-haque <anamul21325@gmail.com>
2022-12-06 22:54:06 +0600

    comment contact

--- pages/contact.js
+++ pages/contact.js
@@ -42,3 +42,16 @@
 // }
 
 // export default contact
+
+
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>
+            contact
+        </div>
+    )
+}
+
+export default contact
